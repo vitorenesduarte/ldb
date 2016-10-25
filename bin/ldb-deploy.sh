fb872f75c102c218a033b48042ce0513665b6a86
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2016-10-25 15:35:58 +0100

    Add ldb dcos deploy script

---
+++ bin/ldb-deploy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+DCOS=$(dcos config show core.dcos_url)
+TOKEN=$(dcos config show core.dcos_acs_token)
+
+ENV_VARS=(
+  BRANCH
+)
+
+for ENV_VAR in "${ENV_VARS[@]}"
+do
+  if [ -z "${!ENV_VAR}" ]; then
+    echo ">>> ${ENV_VAR} is not configured. Please export it."
+    exit 1
+  fi
+done
+
+echo ">>> Configuring LDBs"
+cd /tmp
+
+MEMORY=2048.0
+CPU=2
+
+cat <<EOF > ldbs.json
+{
+  "acceptedResourceRoles": [
+    "slave_public"
+  ],
+  "id": "ldbs",
+  "dependencies": [],
+  "constraints": [["hostname", "UNIQUE", ""]],
+  "cpus": $CPU,
+  "mem": $MEMORY,
+  "instances": 1,
+  "container": {
+    "type": "DOCKER",
+    "docker": {
+      "image": "vitorenesduarte/ldb",
+      "network": "HOST",
+      "forcePullImage": true,
+      "parameters" : [
+        { "key": "oom-kill-disable", "value": "true" }
+      ]
+    }
+  },
+  "ports": [0],
+  "env" : {
+    "BRANCH": "$BRANCH",
+    "DCOS": "$DCOS",
+    "TOKEN": "$TOKEN"
+  },
+  "healthChecks": [
+    {
+      "path": "/api/health",
+      "portIndex": 0,
+      "protocol": "HTTP",
+      "gracePeriodSeconds": 300,
+      "intervalSeconds": 60,
+      "timeoutSeconds": 20,
+      "maxConsecutiveFailures": 3,
+      "ignoreHttp1xx": false
+    }
+  ]
+}
+EOF
+
+echo ">>> Adding LDBs to Marathon"
+curl -s -H "Authorization: token=$TOKEN" -H 'Content-type: application/json' -X POST -d @ldbs.json "$DCOS/service/marathon/v2/apps" > /dev/null
