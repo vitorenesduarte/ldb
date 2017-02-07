ae7776d33fcf0615f11c538b8d99f363d24c5c45
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-02-07 17:51:37 +0000

    Add sim module

---
+++ src/sim.erl
@@ -0,0 +1,69 @@
+%%
+%% Copyright (c) 2016 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(sim).
+-author("Vitor Enes Duarte <vitorenesduarte@gmail.com").
+
+-include("ldb.hrl").
+
+-behaviour(gen_server).
+
+%% sim callbacks
+-export([start_link/0,
+         create_nodes/1]).
+
+%% gen_server callbacks
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+
+-record(state, {nodes :: ordsets::ordset(any())}).
+
+-spec start_link() -> {ok, pid()} | ignore | {error, term()}.
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+-spec create_nodes(list(term())) -> ok.
+create_nodes(Nodes) ->
+    gen_server:call(?MODULE, {create_nodes, Nodes}).
+
+%% gen_server callbacks
+init([]) ->
+    ldb_log:info("sim initialized!"),
+    {ok, #state{}}.
+
+handle_call({create_nodes, _Nodes}, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(Msg, State) ->
+    ldb_log:warning("Unhandled cast message: ~p", [Msg]),
+    {noreply, State}.
+
+handle_info(Msg, State) ->
+    ldb_log:warning("Unhandled info message: ~p", [Msg]),
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
