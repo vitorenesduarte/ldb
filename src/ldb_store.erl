6ce8e5f8289c172a9b4ea81f0e6ba807d16803fd
Vitor Enes <vitorenesduarte@gmail.com>
2018-09-25 20:02:20 +0100

    Change copyright

--- src/ldb_store.erl
+++ src/ldb_store.erl
@@ -1,5 +1,5 @@
 %%
-%% Copyright (c) 2016 SyncFree Consortium.  All Rights Reserved.
+%% Copyright (c) 2016-2018 Vitor Enes.  All Rights Reserved.
 %% Copyright (c) 2016 Christopher Meiklejohn.  All Rights Reserved.
 %%
 %% This file is provided to you under the Apache License,
